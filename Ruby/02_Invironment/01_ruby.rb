b6079da19d16b1df0a5513f62e1ce640cf55d248
kjmkagaya <jm.kagaya@gmail.com>
2018-06-12 00:56:12 +0900

    Done
    Done

---
+++ Ruby/02_Invironment/01_ruby.rb
@@ -0,0 +1,57 @@
+
+
+
+=begin
+    
+
+rescue => exception
+    
+    Ruby - Environment Setup
+Advertisements
+ Previous Page Next Page  
+Local Environment Setup
+If you are still willing to set up your environment for Ruby programming language, then let's proceed. This tutorial will teach you all the important topics related to environment setup. We would recommend you to go through the following topics first and then proceed further −
+
+Ruby Installation on Linux/Unix − If you are planning to have your development environment on Linux/Unix Machine, then go through this chapter.
+
+Ruby Installation on Windows − If you are planning to have your development environment on Windows Machine, then go through this chapter.
+
+Ruby Command Line Options − This chapter list out all the command line options, which you can use along with Ruby interpreter.
+
+Ruby Environment Variables − This chapter has a list of all the important environment variables to be set to make Ruby Interpreter works.
+
+Popular Ruby Editors
+To write your Ruby programs, you will need an editor −
+
+If you are working on Windows machine, then you can use any simple text editor like Notepad or Edit plus.
+
+VIM (Vi IMproved) is a very simple text editor. This is available on almost all Unix machines and now Windows as well. Otherwise, your can use your favorite vi editor to write Ruby programs.
+
+RubyWin is a Ruby Integrated Development Environment (IDE) for Windows.
+
+Ruby Development Environment (RDE) is also a very good IDE for windows users.
+
+Interactive Ruby (IRb)
+Interactive Ruby (IRb) provides a shell for experimentation. Within the IRb shell, you can immediately view expression results, line by line.
+
+This tool comes along with Ruby installation so you have nothing to do extra to have IRb working.
+
+Just type irb at your command prompt and an Interactive Ruby Session will start as given below −
+
+$irb
+irb 0.6.1(99/09/16)
+irb(main):001:0> def hello
+irb(main):002:1> out = "Hello World"
+irb(main):003:1> puts out
+irb(main):004:1> end
+nil
+irb(main):005:0> hello
+Hello World
+nil
+irb(main):006:0>
+Do not worry about what we did here. You will learn all these steps in subsequent chapters.
+
+What is Next?
+We assume now you have a working Ruby Environment and you are ready to write the first Ruby Program. The next chapter will teach you how to write Ruby programs.
+
+end
